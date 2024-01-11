b81a327e8fabd3151df50d67fb9556878cd3f6c6
beta-j <60655500+beta-j@users.noreply.github.com>
2024-01-11 13:40:40 +0100

    Update get_nonces.py

--- code/get_nonces.py
+++ code/get_nonces.py
@@ -8,22 +8,3 @@ with open('official_public.pm', ‘rb’)as fh:
 for x in range (0,1548):
   nonce= c2.blocks[x].nonce
   print (nonce)
-
-predict_nonces.py
-
-#!/usr/bin/env python3
-import random
-from mt19937predictor import MT19937Predictor
-
-predictor = MT19937Predictor()		# load the Mersenne Twister Predictor
-file = open("outfile.txt", "r")		# load the file with the nonces we got with get_nonces.py
-content =  file.readlines()		
-
-for line in range (0,1548):		# cycle through the file line by line
-    
-    x = int(content[line])		# read the value of each line and convert it to integer
-    print (line, ": ",x)
-    predictor.setrandbits(x, 64)		# teach the value to the predictor
-
-for i in range (129997, 130001):
-  print (i,": ",predictor.getrandbits(64))  # predict the values for indexes 129997 to 130000
